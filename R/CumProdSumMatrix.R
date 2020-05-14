e082b636372de227d58a2c2c60256c3a0d73de55
Ben <blangwor@live.unc.edu>
2020-05-14 16:41:09 -0400

    Updated description

--- R/CumProdSumMatrix.R
+++ R/CumProdSumMatrix.R
@@ -1,3 +1,4 @@
+#Modified from code received from Yu Cheng at University of Pittsburgh.
 # subfunctions to calculate cumulative sum function of a matrix
 cummatrix = function(ee)
 {n2 = dim(ee)[1]
@@ -5,7 +6,6 @@ n4 = dim(ee)[2]
 
 for (i in 1:n4)
   ee[,i] = cumsum(ee[,i])
-
 for (i in 1:n2)
   ee[i,] = cumsum(ee[i,])
 return(ee)
