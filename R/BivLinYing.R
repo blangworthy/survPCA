eb35d87aa6a69f76d47e4d6e9c0987763c41f0a9
blangworthy <langworthy.ben@gmail.com>
2020-09-17 14:21:22 -0400

    Speed up Lin Ying estimator

--- R/BivLinYing.R
+++ R/BivLinYing.R
@@ -7,19 +7,31 @@
 BivLinYing <- function(data){
   data <- as.matrix(data)
   if(sum(is.na(data)) >0){stop("Data includes missing values")}
-  
+  ndim = dim(data)[1]
   uniquetime1 = unique(sort(data[,1]))
   uniquetime2 = unique(sort(data[,2]))
+  
+  npts1 = length(uniquetime1)
+  npts2 = length(uniquetime2)
+  
+  
+  tmp1 = matrix(rep(data[,1],each = npts1),npts1,ndim)
+  tmp2 = matrix(rep(data[,2],each = npts2),npts2,ndim)
+  
+  esurv <-  ifelse(tmp1>=uniquetime1,1,0)%*%t(ifelse(tmp2>=uniquetime2,1,0))/ndim
   
   censortime <- apply(data,1,function(x) max(x[1],x[2]))
   censorind <- apply(data,1,function(x) x[3]*x[4])
   
   censkm <- survival::survfit(survival::Surv(censortime,(1-censorind))~1)
   censsurv <- stepfun(censkm$time, c(1, censkm$surv))
-
   
+  surv1 <- censsurv(uniquetime1)
+  surv2 <- censsurv(uniquetime2)
+  censsurvmat <- sapply(surv2,function(x) sapply(surv1,function(y) min(x,y)))
   
-  mat <- sapply(uniquetime2,function(x) sapply(uniquetime1, function(y) ifelse(censsurv(max(x,y))>0,mean(data[,2]>x & data[,1] > y)/censsurv(max(x,y)),NA)))
+  mat <- esurv/censsurvmat + 0*esurv/censsurvmat ###Second part is to get NA instead of Inf
+  
   rownames(mat) <- uniquetime1
   colnames(mat) <- uniquetime2
   return(mat)
